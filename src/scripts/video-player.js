42d61856c805f3414d2e5b2ada64b967da875fde
Matheus Brasil <matheus.brasil10@gmail.com>
2016-10-18 20:54:53 -0200

    Setup teaser

--- src/scripts/video-player.js
+++ src/scripts/video-player.js
@@ -11,9 +11,9 @@
     const playerElement = document.getElementById("player");
 
     const player = new Clappr.Player({
-      source: 'http://clappr.io/highline.mp4',
+      source: './../videos/blau-teaser.mp4',
       baseUrl: '/latest',
-      poster: 'http://clappr.io/poster.png',
+      poster: './../videos/blau-teaser-poster.png',
       height: "100%",
       width: "100%",
       mediacontrol: {seekbar: "#000000", buttons: "#000000"}
